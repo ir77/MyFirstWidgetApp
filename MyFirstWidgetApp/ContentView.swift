7d3ba7cb6aaad6c82a3c10c95bdcc81a7adabec4
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2020-09-03 23:31:03 +0900

    Initial Commit

---
+++ MyFirstWidgetApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFirstWidgetApp
+//
+//  Created by ucuc on 2020/09/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFirstWidgetApp/MyFirstWidgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstWidgetAppApp.swift
+//  MyFirstWidgetApp
+//
+//  Created by ucuc on 2020/09/03.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstWidgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
